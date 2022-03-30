6445495342e890b89523a92d493ad38b8c56039e
Giuseppe Colavito <giuseppecolavito999@gmail.com>
2022-03-30 10:50:38 +0200

    Update table.py

--- src/table.py
+++ src/table.py
@@ -8,7 +8,7 @@ BRIGHT_COLORS = set(['bright_' + c for c in list(COLORS)])
 
 def create_rich_table(table: pd.DataFrame, title: str, box_style: box = box.ROUNDED, show_lines: bool = True):
     colors = BRIGHT_COLORS.copy()
-    header = [Column(header=Name, justify='right', style=colors.pop(), no_wrap=True) for Name in table.columns]
+    header = [Column(header=Name, justify='right', style=colors.pop()) for Name in table.columns]
     rich_table = Table(title=title, box=box_style, header_style='bold', show_lines=show_lines, title_style='bold', *header)
     for row in table.values:
         rich_table.add_row(*[str(x) for x in row])
