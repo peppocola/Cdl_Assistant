d8c8a3437f7d49f3354211cc6785574be975489a
peppocola <giuseppecolavito999@gmail.com>
2022-04-01 12:38:32 +0200

    add bash scripts

---
+++ bash_local.sh
@@ -0,0 +1,11 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install python3.10 -y
+sudo add-apt-repository ppa:swi-prolog/stable -y
+sudo apt-get install swi-prolog -y
+sudo apt-get install python3.10-distutils -y
+wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py
+python3.10 get-pip.py
+python3.10 -m pip install -r requirements.txt
+cd src
+python3.10 intent_detection.py
